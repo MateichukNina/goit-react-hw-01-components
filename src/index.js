b15bd33620f2c7d9a43f91dc4473623e06c8deed
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2023-08-04 19:00:07 +0300

    update

--- src/index.js
+++ src/index.js
@@ -5,25 +5,6 @@ import './index.css';
 import App from './App';
 //  import reportWebVitals from './reportWebVitals';
 
-// const elem1 = <span>Hello</span>;
-// const elem2 = <span>world</span>;
-// const element = (
-//   <div>
-//     {elem1}
-//     {elem2}
-//   </div>
-// );
-
-// console.log(element);
-
-// ReactDOM.render(element, document.querySelector('#root'));
-
-
-//  import React from 'react';
-//  import ReactDOM from 'react-dom/client';
-//  import './index.css';
-//  import App from './App';
-//  import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render(
@@ -31,8 +12,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
      <App />
    </React.StrictMode>
  );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
